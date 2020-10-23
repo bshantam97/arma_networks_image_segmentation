4ccfb7fce879a7f85295fd6c3d5cac838a23dc20
Arpit Aggarwal <arpitdec5@gmail.com>
2020-10-23 19:41:04 -0400

    Worked on the implementation of the backbone of deeplabv3, modified files in code/ folder

--- code/network/backbone/resnet.py
+++ code/network/backbone/resnet.py
@@ -1,36 +1,55 @@
+# header files
 import torch
 import torch.nn as nn
 from torchvision.models.utils import load_state_dict_from_url
+from arma import *
 
 
+# required functions
 __all__ = ['resnet34', 'resnet50', 'resnet101']
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1, arma=False):
+    """
+        in_planes: number of input channels
+        out_planes: number of output channels
+        stride: the stride value
+        groups: the number of groups
+        dilation: the gap between kernel cells
+        arma: True, then arma layer applied, otherwise conv layer
+    """
+    if arma:
+      return ARMA2d(in_planes, out_planes, w_kernel_size=3, w_padding=dilation, w_stride=stride, w_groups=groups, w_dilation=dilation, bias=False)
+    else:
+      return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
 
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+def conv1x1(in_planes, out_planes, stride=1, arma=False):
+    """
+        in_planes: number of input channels
+        out_planes: number of output channels
+        stride: the stride value
+        arma: True, then arma layer applied, otherwise conv layer
+    """
+    if arma:
+      return ARMA2d(in_planes, out_planes, w_kernel_size=1, w_stride=stride, w_padding=0, bias=False)
+    else:
+      return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
 class BasicBlock(nn.Module):
     expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1, norm_layer=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1, norm_layer=None, arma=False):
         super(BasicBlock, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         if groups != 1 or base_width != 64:
             raise ValueError('BasicBlock only supports groups=1 and base_width=64')
         if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
+            raise NotImplementedError("Dilation>1 not supported in BasicBlock")
+
+        self.conv1 = conv3x3(inplanes, planes, stride, arma=arma)
         self.bn1 = norm_layer(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
+        self.conv2 = conv3x3(planes, planes, arma=arma)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
@@ -51,19 +70,18 @@ class BasicBlock(nn.Module):
 
 class Bottleneck(nn.Module):
     expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1, norm_layer=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1, norm_layer=None, arma=False):
         super(Bottleneck, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
-        width = int(planes * (base_width/64.))*groups
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, width)
+        width = int(planes*(base_width/64.))*groups
+        
+        self.conv1 = conv1x1(inplanes, width, arma=arma)
         self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation, arma=arma)
         self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes*self.expansion)
-        self.bn3 = norm_layer(planes*self.expansion)
+        self.conv3 = conv1x1(width, planes*4, arma=arma)
+        self.bn3 = norm_layer(planes*4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -86,7 +104,6 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-
     def __init__(self, block, layers, num_classes=19, zero_init_residual=False, groups=1, width_per_group=64, replace_stride_with_dilation=None, norm_layer=None, arma=False):
         super(ResNet, self).__init__()
         if norm_layer is None:
@@ -96,21 +113,22 @@ class ResNet(nn.Module):
         self.inplanes = 64
         self.dilation = 1
         if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
             replace_stride_with_dilation = [False, False, False]
         if len(replace_stride_with_dilation) != 3:
             raise ValueError("replace_stride_with_dilation should be None or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        if(arma):
+            self.conv1 = ARMA2d(3, self.inplanes, w_kernel_size=7, w_stride=2, w_padding=3, bias=False)
+        else:
+            self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2])
+        self.layer1 = self._make_layer(block, 64, layers[0], arma=arma)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0], arma=arma)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1], arma=arma)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2], arma=arma)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512*block.expansion, num_classes)
 
@@ -131,24 +149,24 @@ class ResNet(nn.Module):
                 elif isinstance(m, BasicBlock):
                     nn.init.constant_(m.bn2.weight, 0)
 
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False, arma=False):
         norm_layer = self._norm_layer
         downsample = None
         previous_dilation = self.dilation
         if dilate:
             self.dilation *= stride
             stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
+        if stride != 1 or self.inplanes != planes*block.expansion:
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
+                conv1x1(self.inplanes, planes*block.expansion, stride, arma=arma),
+                norm_layer(planes*block.expansion),
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer))
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer, arma=arma))
         self.inplanes = planes*block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups, base_width=self.base_width, dilation=self.dilation, norm_layer=norm_layer))
+            layers.append(block(self.inplanes, planes, groups=self.groups, base_width=self.base_width, dilation=self.dilation, norm_layer=norm_layer, arma=arma))
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -166,36 +184,36 @@ class ResNet(nn.Module):
         return x
 
 
-def _resnet(arch, block, layers, progress, **kwargs, arma=False):
-    model = ResNet(block, layers, **kwargs, arma=arma)
+def _resnet(arch, block, layers, progress, arma=False, **kwargs):
+    model = ResNet(block, layers, arma=arma, **kwargs)
     return model
 
 
-def resnet34(pretrained=False, progress=True, **kwargs, arma=False):
+def resnet34(pretrained=False, progress=True, arma=False, **kwargs):
     r"""ResNet-34 model from
     `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _resnet('resnet34', BasicBlock, [3, 4, 6, 3], progress, **kwargs, arma=arma)
+    return _resnet('resnet34', BasicBlock, [3, 4, 6, 3], progress, arma=arma, **kwargs)
 
 
-def resnet50(pretrained=False, progress=True, **kwargs, arma=False):
+def resnet50(pretrained=False, progress=True, arma=False, **kwargs):
     r"""ResNet-50 model from
     `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], progress, **kwargs, arma=arma)
+    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], progress, arma=arma, **kwargs)
 
 
-def resnet101(pretrained=False, progress=True, **kwargs, arma=False):
+def resnet101(pretrained=False, progress=True, arma=False, **kwargs):
     r"""ResNet-101 model from
     `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], progress, **kwargs, arma=arma)
+    return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], progress, arma=arma, **kwargs)
